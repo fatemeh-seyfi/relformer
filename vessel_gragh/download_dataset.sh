f44486c2eff4219646f94af64d76d73dfc63f0f1
fatemeh-seyfi <143171841+fatemeh-seyfi@users.noreply.github.com>
2023-11-08 21:14:11 +0330

    Update download_dataset.sh

--- vessel_gragh/download_dataset.sh
+++ vessel_gragh/download_dataset.sh
@@ -1,19 +1,21 @@
-FILE=$1
-if [[ $FILE != "raw"  ]]; then
-    echo "Available datasets are:raw "
-    exit 1
-fi
-if [[ $FILE == "cityscapes" ]]; then
-    echo "Due to license issue, we cannot provide the Cityscapes dataset from our repository. Please download the Cityscapes dataset from https://cityscapes-dataset.com, and use the script ./datasets/prepare_cityscapes_dataset.py."
-    echo "You need to download gtFine_trainvaltest.zip and leftImg8bit_trainvaltest.zip. For further instruction, please read ./datasets/prepare_cityscapes_dataset.py"
-    exit 1
-fi
-echo "Specified [$FILE]"
-URL=https://syncandshare.lrz.de/dl/fiA6KKF6AAzux7Pw2ANnBnLf/$FILE.zip
-ZIP_FILE=./data/vessel_data/$FILE.zip
-TARGET_DIR=./data/vessel_data/$FILE/
-wget -N $URL -O $ZIP_FILE
-mkdir $TARGET_DIR
-unzip $ZIP_FILE -d ./data/vessel_data/
-rm $ZIP_FILE
+# FILE=$1
+# if [[ $FILE != "raw"  ]]; then
+#     echo "Available datasets are:raw "
+#     exit 1
+# fi
+# if [[ $FILE == "cityscapes" ]]; then
+#     echo "Due to license issue, we cannot provide the Cityscapes dataset from our repository. Please download the Cityscapes dataset from https://cityscapes-dataset.com, and use the script ./datasets/prepare_cityscapes_dataset.py."
+#     echo "You need to download gtFine_trainvaltest.zip and leftImg8bit_trainvaltest.zip. For further instruction, please read ./datasets/prepare_cityscapes_dataset.py"
+#     exit 1
+# fi
+# echo "Specified [$FILE]"
+# URL=https://syncandshare.lrz.de/dl/fiA6KKF6AAzux7Pw2ANnBnLf/$FILE.zip
+# ZIP_FILE=./data/vessel_data/$FILE.zip
+# TARGET_DIR=./data/vessel_data/$FILE/
+# wget -N $URL -O $ZIP_FILE
+# mkdir $TARGET_DIR
+# unzip $ZIP_FILE -d ./data/vessel_data/
+# rm $ZIP_FILE
+wget https://syncandshare.lrz.de/dl/fiA6KKF6AAzux7Pw2ANnBnLf/raw.zip
+unzip raw.zip 
 
